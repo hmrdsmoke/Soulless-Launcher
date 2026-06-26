4d84183db3541a12d2f77ab6ccd39a65e752de89
hmrdsmoke <m.r.vanauker07@gmail.com>
2026-06-26 02:59:28 -0400

    feat: re-enable hardware_monitor subscription

--- launcher/src/app.rs
+++ launcher/src/app.rs
@@ -507,7 +507,7 @@ impl cosmic::Application for Soulless {
             // soulless_organizer::subscription().map(Message::Organizer),
             // crate::network_monitor::subscription().map(Message::Network),
             crate::system_monitor::subscription().map(Message::System),
-            // crate::hardware_monitor::subscription().map(Message::Hardware),
+            crate::hardware_monitor::subscription().map(Message::Hardware),
             crate::fps_monitor::subscription().map(Message::Fps),
         ])
     }
